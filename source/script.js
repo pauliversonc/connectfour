0eb5acafd7423df659f1dd0a356de844beffe905
Paul Iverson Cortez <33085670+pauliversonc@users.noreply.github.com>
2021-06-09 15:36:43 +0800

    added jquery cdn and script

---
+++ source/script.js
@@ -0,0 +1,8 @@
+var player1 = prompt("Player 1: Enter your name, You will be Blue");
+var player1Color = 'rgb(69, 151, 255)';
+
+var player2 = prompt("Player 2: Enter your name, You will be Red");
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
