2491708b6efd953ee1f75ff781a4d0c5f26c9871
Paul Iverson Cortez <33085670+pauliversonc@users.noreply.github.com>
2021-06-10 15:50:48 +0800

    add logic in game

--- source/script.js
+++ source/script.js
@@ -1,8 +1,8 @@
-var player1 = 'Paul Cortez';
+// var player1 = 'Paul Cortez';
 var player1 = prompt("Player One: Enter your name, you will be Blue");
 var player1Color = 'rgb(86, 151, 255)';
 
-var player2 = 'Iverson Cortez';
+// var player2 = 'Iverson Cortez';
 var player2 = prompt("Player Two: Enter your name, you will be Red");
 var player2Color = 'rgb(237, 45, 73)';
 
